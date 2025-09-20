46e6787dfb095705dd3b456fb4761c5e4873ac4b
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-09-21 01:51:10 +0530

    Add hierarchial filtering boilerplate

---
+++ src/cpu/h_blur.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <chrono>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+
+// g++ h_blur.cpp -o h_blur $(pkg-config --cflags --libs opencv4)
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		std::cout << "ERR: Please provide an image path!" << std::endl;
+		return 1;
+	}
+	cv::Mat original_image = cv::imread(argv[1]);
+	if (original_image.empty()) {
+		std::cout << "ERR: Img data empty" << std::endl;
+		return 2;
+	}
+	cv::Mat filtered_image;
+	int kernelsize = 19;
+	
+    // the timing section
+	auto start = std::chrono::high_resolution_clock::now();
+
+    // replace with new func
+	cv::medianBlur(original_image, filtered_image, kernelsize);
+
+	auto end = std::chrono::high_resolution_clock::now();
+    // end of func
+
+    
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+	std::cout << duration.count() << "ms was taken" << std::endl;
+
+	cv::imshow("Original", original_image);
+	cv::imshow("Median Filtered", filtered_image);
+
+	bool saved = cv::imwrite("../results/filtered_output_cv.jpg", filtered_image);
+	if (saved) {
+		std::cout << "Saved!" << std::endl;
+	} else {
+		std::cout << "Error while saving." << std::endl;
+	}
+	cv::waitKey(0);
+	return 0;
+}
